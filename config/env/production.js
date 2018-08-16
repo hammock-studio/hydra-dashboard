4adfcb134fd7b41517e513ab1352f8cdd0a27bf6
karopastal <tpaskaro@gmail.com>
2018-08-17 00:38:56 +0300

    add production config

--- config/env/production.js
+++ config/env/production.js
@@ -3,5 +3,16 @@
 //
 module.exports = {
   env: "production",
-//   db: sequelize,
+  // db: sequelize,
+  hydra: {
+    "client": {
+      "id": process.env.HYDRA_DASHBOARD_CLIENT_ID,
+      "secret": process.env.HYDRA_DASHBOARD_CLIENT_SECRET
+    },
+    "auth": {
+      "tokenHost": process.env.HYDRA_URL,
+      "authorizePath": "/oauth2/auth",
+      "tokenPath": "/oauth2/token"
+    }
+  }
 };
