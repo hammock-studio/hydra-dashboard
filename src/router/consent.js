be3fd63a3df1f0e82981ae4ceca4f3be084223b2
karopastal <tpaskaro@gmail.com>
2018-08-17 00:28:28 +0300

    basic auth setup

---
+++ src/router/consent.js
@@ -0,0 +1,73 @@
+const express = require('express');
+
+const router = express.Router();
+const hydra = require('../helpers/hydra');
+
+const sessionChecker = (req, res, next) => {
+  next();
+};
+
+router.route('/').get(sessionChecker, (req, res, next) => {
+  const challenge = req.query.consent_challenge;
+
+  hydra.getConsentRequest(challenge).then((response) => {
+    if (response.skip) {
+      const body = {
+        grant_scope: response.requested_scope,
+        session: {}
+      };
+
+      hydra.acceptConsentRequest(challenge, body).then((acceptResponse) => {
+        res.redirect(acceptResponse.redirect_to);
+      });
+    }
+
+    res.render('consent', {
+      challenge,
+      requested_scope: response.requested_scope,
+      user: response.subject,
+      client: response.client
+    });
+  }).catch((error) => {
+    next(error);
+  });
+});
+
+router.route('/').post(sessionChecker, (req, res, next) => {
+  let grantScope;
+  const { challenge } = req.body;
+
+  if (req.body.submit === 'Deny access') {
+    const body = {
+      error: 'access_denied',
+      error_description: 'The resource owner denied the request'
+    };
+
+    hydra.rejectConsentRequest(req.body.challenge, body).then((response) => {
+      res.redirect(response.redirect_to);
+    }).catch((error) => {
+      next(error);
+    });
+  }
+
+  grantScope = req.body.grant_scope;
+
+  if (!Array.isArray(grantScope)) {
+    grantScope = [grantScope];
+  }
+
+  const body = {
+    grant_scope: grantScope,
+    session: {},
+    remember: Boolean(req.body.remember),
+    remember_for: 3600
+  };
+
+  hydra.acceptConsentRequest(challenge, body).then((response) => {
+    res.redirect(response.redirect_to);
+  }).catch((error) => {
+    next(error);
+  });
+});
+
+module.exports = router;
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,42 @@
 const express = require('express');
+const hydra = require('../helpers/hydra');
 
 const router = express.Router();
+
+router.use('/login', require('./login'));
+router.use('/consent', require('./consent'));
+router.use('/dashboard', require('./dashboard'));
 
 const sessionChecker = (req, res, next) => {
   next();
 };
 
 router.get('/', sessionChecker, (req, res) => {
-  res.json({ loc: 'Anna Bunanaaaaa :)' });
+  res.json({
+    loc: 'this is the dashboard home page',
+    authorize_dasboard: hydra.getAuthorizationURI()
+  });
 });
+
+router.get('/callback', (req, res) => {
+  hydra.exchangeCodeForToken(req.query.code, (error, response) => {
+    if (error) { res.json({ error }); }
+
+    req.session.user = { tokens: response.token };
+
+    res.redirect('/dashboard');
+  });
+});
+
+
+// req.session.user.access_token = token.token.access_token;
+// req.session.user.refresh_token = token.token.refresh_token;
+
+// if (!true) {
+// res.redirect('/admin')
+// } else {
+// res.json({tokens: tokens})
+// res.redirect('/dashboard');
+// }
 
 module.exports = router;
