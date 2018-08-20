e08c77a6f63fad8e6e14a1eebaf60a54bf8bee14
karopastal <tpaskaro@gmail.com>
2018-08-20 13:37:55 +0300

    change PG settings

--- config/env/production.js
+++ config/env/production.js
@@ -1,14 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(
-  process.env.PG_DATABASE,
-  process.env.PG_USERNAME,
-  process.env.PG_PASSWORD,
-  {
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    dialect: 'postgres'
-  }
-);
+const sequelize = new Sequelize(process.env.PG_URL);
 
 module.exports = {
   env: "production",
