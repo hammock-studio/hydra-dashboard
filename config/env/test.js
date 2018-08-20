6892f45d2d990e8f1efe3d1f2e0685054bfb1b6c
karopastal <tpaskaro@gmail.com>
2018-08-20 13:49:07 +0300

    change test env config loading

--- config/env/test.js
+++ config/env/test.js
@@ -1,7 +1,11 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.PG_TEST_URL);
+if (process.env.NODE_ENV == 'test'){
+  const Sequelize = require('sequelize');
+  const sequelize = new Sequelize(process.env.PG_TEST_URL);
 
-module.exports = {
-  env: "test",
-  db: sequelize,
+  module.exports = {
+    env: 'test',
+    db: sequelize,
+  }
+} else {
+  module,exports = { env: 'test'}
 }
