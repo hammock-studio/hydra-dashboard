51473e991a478c9366e37baa92027e2fd99d6998
karopastal <tpaskaro@gmail.com>
2018-08-20 13:19:53 +0300

    add sequilize conf

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,14 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.PG_URL, null, null, { dialect: 'postgres' });
+const sequelize = new Sequelize(
+  process.env.PG_DATABASE,
+  process.env.PG_USERNAME,
+  process.env.PG_PASSWORD,
+  {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    dialect: 'postgres'
+  }
+);
 
 module.exports = {
   env: "production",
